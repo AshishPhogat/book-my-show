2ff76a698b0c3755600ed38b14fc3f2b8ab237ed
AshishPhogat <ashishphog@gmail.com>
2021-09-01 12:13:08 +0530

    learnt to use className in react-tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App(){
+  return <>
+  <div className= " px-10">
+    <h1 className= " text-6xl hover:text-blue-500"> hello everyone</h1>
+    <h1 className= " text-6xl hover:text-blue-50"> hello everyone</h1>
+  </div>
+
+  <button className= "mx-10  border-4 border-blue-500 px-5 py-2 text-lg text-white rounded-lg bg-blue-500  focus:outline-none focus:ring focus:border-blue-300 ">
+    hover me!
+  </button>
+
+  </>
+}
+
+export default App;
