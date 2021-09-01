8fb4f73f79bce1925ca556a3302a4a2e1a4e824a
AshishPhogat <ashishphog@gmail.com>
2021-09-01 13:23:46 +0530

    customised color

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App(){
   return <>
   <div className= " px-10 md:hover:bg-blue-900   ">
-    <h1 className= " text-6xl hover:text-blue-500 md:hover:text-pink-500"> hello everyone</h1>
-    <h1 className= " text-6xl hover:text-blue-50 md:hover:text-pink-50"> hello everyone</h1>
+    <h1 className= " text-6xl hover:text-blue-500 md:hover:text-Ashish-500"> hello everyone</h1>
+    <h1 className= " text-6xl hover:text-blue-50 md:hover:text-Ashish-50"> hello everyone</h1>
   </div>
 
   <button className= "mx-10  border-4 border-blue-500 md:border-red-500 md:bg-red-500 md:text-white lg:border-green-500 lg:bg-green-500 px-5 py-2 text-lg text-white rounded-lg bg-blue-500   focus:outline-none focus:ring-4 focus:ring-green-500 lg:focus:ring-blue-500   ">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        Ashish: 
+        {
+          50: '#fff1e5',
+          100: '#fbd5b8',
+          200: '#fab987',
+          300: '#fa9d56',
+          400: '#fa8129',
+          500: '#e16915',
+          600: '#af510f',
+          700: '#7d3a09',
+          800: '#4b2306',
+          900: '#1a0b00',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
