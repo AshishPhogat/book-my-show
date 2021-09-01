6a9ab4b21139f7e20a10b78f535e9dc67af0a838
AshishPhogat <ashishphog@gmail.com>
2021-09-01 21:38:24 +0530

    planned for the project

---
+++ planning.js
@@ -0,0 +1,28 @@
+// BOOKMyShow planning
+
+//->we need reusable carousal
+//  ->Hero Carousal
+//  ->Common carousal
+//  ->Cast and crew small carousal
+
+
+
+//pages to be built
+
+// ->Home page
+// ->Movie page
+// ->Categories page
+// ->Auth page
+// ->Razorpay payment gateway
+
+//Plan ->How are we building it?🧐
+// Layouts
+// Routes
+// Pages
+// Components
+
+
+
+
+
+
