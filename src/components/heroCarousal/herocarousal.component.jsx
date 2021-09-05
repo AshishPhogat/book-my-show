a166235ec5a11bec1449e1f0908ee8774c4a4aff
AshishPhogat <ashishphog@gmail.com>
2021-09-05 09:29:07 +0530

    made carousal responsive

--- src/components/heroCarousal/herocarousal.component.jsx
+++ src/components/heroCarousal/herocarousal.component.jsx
@@ -6,16 +6,23 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const HeroCarousal = ()=>{
-
+    
+    const settingsLg={
+        arrows:true,
+        autoplay:true,
+        centerMode:true,
+        centerPadding:"300px",
+        slidesToScroll:1,
+        slidesToShow:1,
+        infinite: true,
+        speed:500,
+    };
     const settings = {
         dots: true,
         arrows:true,
         infinite: true,
         speed: 500,
-        centerPadding:"200px",
         slidesToShow: 1,
-        innerWidth:"3/4",
-        centerMode:true,
         slidesToScroll: 1
       };
       const images=["https://images.unsplash.com/photo-1593642532781-03e79bf5bec2?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
@@ -24,13 +31,26 @@ const HeroCarousal = ()=>{
                     "https://images.unsplash.com/photo-1630683392459-f316bea8e7ed?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"];
 
     return <> 
+
+        <div className="lg:hidden">
         <HeroSlider  {...settings}>
             {images.map((image)=>{
-                return (<div className="h-96">
-                    <img src={image} alt="testing" className="w-full h-full" />
+                return (<div className="w-full h-52 md:h-96 py-3">
+                    <img src={image} alt="testing" className="w-full h-full lg:rounded-sm" />
                 </div>)
             })}
         </HeroSlider>
+        </div>
+        <div className="hidden lg:block">
+        <HeroSlider  {...settingsLg }>
+            {images.map((image)=>{
+                return (<div className="w-full px-2 h-80">
+                    <img src={image} alt="testing" className="w-full h-full" />
+                </div>)
+            })}
+        </HeroSlider> 
+        </div>
+        
 
     </>
 };
