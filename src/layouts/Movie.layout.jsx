ece65089ad5d1a23b766c8c20cb1a078e48a3ce4
AshishPhogat <ashishphog@gmail.com>
2021-09-26 09:47:35 +0530

    implemented the useContext

--- src/layouts/Movie.layout.jsx
+++ src/layouts/Movie.layout.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import Reactj,{useEffect,useContext} from "react";
+import {useParams} from "react-router-dom";
+
+
 //Components
 import MovieNavbar from "../components/Navbar/movienavbar.component";
 
+//context
+import { MovieContext } from "../context/movie.context";
+
+
 const MovieLayout=(props)=>{
+
+    const {id} = useParams();
+
     return <> 
         <MovieNavbar /> 
         {props.children}
